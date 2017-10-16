1017cae7f68dcbb85c000d4cab3389217cab0972
Louise Brown <louise.brown@nottingham.ac.uk>
2017-10-16 18:05:33 +0100

    Added 2DTextile script

---
+++ 2DTextile.py
@@ -0,0 +1,18 @@
+# 2DTextile.py
+
+# Specify weave parameters
+nwarp=2   #Number of weft yarns in the unit cell
+nweft=2    #Number of warp yarns in the unit cell
+s=1          #Spacing between the yarns
+t=0.1       #Thickness of the fabric (sum of two yarn heights)
+ref=True  #Refine model (True/False)
+
+# Create 2D textile
+weave = CTextileWeave2D( nweft, nwarp, s, t, ref )
+
+# Set the weave pattern
+weave.SwapPosition(0, 0)
+weave.SwapPosition(1, 1)
+
+#Add to the textile database
+AddTextile(weave)
