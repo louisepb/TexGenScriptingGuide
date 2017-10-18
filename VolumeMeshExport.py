4412ba5b3319c67373b7f569bb1c92a1eec61bd2
Louise Brown <louise.brown@nottingham.ac.uk>
2017-10-18 12:24:37 +0100

    Added comments

--- VolumeMeshExport.py
+++ VolumeMeshExport.py
@@ -1,4 +1,4 @@
-# 2DTextile.py
+# VolumeMeshExport.py
 
 # Specify weave parameters
 nwarp=2   #Number of weft yarns in the unit cell
@@ -14,14 +14,19 @@ weave = CTextileWeave2D( nweft, nwarp, s, t, ref )
 weave.SwapPosition(0, 0)
 weave.SwapPosition(1, 1)
 
+# Assign a default domain
 weave.AssignDefaultDomain()
+
 #Add to the textile database
 AddTextile(weave)
 
+# Create CMesher object and set periodicity and seed size
 mesher = CMesher(MATERIAL_CONTINUUM)
 mesher.SetPeriodic(True)
 mesher.SetSeed(0.1)
+# Generate the mesh
 mesher.CreateMesh(weave)
+# Save the mesh to Abaqus
 mesher.SaveVolumeMeshToABAQUS("VolumeMesh.inp", weave.GetName())
 
 
